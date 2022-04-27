57f2de42b3e767e167b9c7047e313c76bffe5843
gmoulis <70227120+gmoulis@users.noreply.github.com>
2022-04-27 18:37:20 +0200

    day1

---
+++ day1.py
@@ -0,0 +1,35 @@
+#Print
+print("Hello, SQA batch 11")
+
+x = 5
+y = "Student"
+print(x*10)
+print(y)
+
+#age= "30"
+age = 30
+y = "Hello, my age is "
+print(y + str(age))
+
+
+z = "7"
+a = int(z)
+print(a)
+
+#Type of object
+b = 6
+c = "Hi"
+d = True
+e = 5,9
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+#Sensible aux majuscules
+f = 20
+F = 30
+print(f)
+print(F)
+
+
