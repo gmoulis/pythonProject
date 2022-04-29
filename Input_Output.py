6cca8ab9e008c382bdf2a138b9e86780186b2384
Ganelle <70227120+gmoulis@users.noreply.github.com>
2022-04-29 16:03:46 +0200

    Add files via upload

---
+++ Input_Output.py
@@ -0,0 +1,17 @@
+num1 = input("Enter 1st number: ")
+num2 = input("Enter 2nd number: ")
+print("Number 1: " + num1)
+print("Number 2: " + num2)
+
+name = input ("What is your name?")
+print("My name is " + name)
+
+num1 = input("Enter 1st number: ")
+num2 = input("Enter 2nd number: ")
+result = int(num1) * int(num2)
+print("Multiplication of two numbers: " + str(result))
+
+num1 = input("Enter 1st number: ")
+num2 = input("Enter 2nd number: ")
+result = int(num1) * int(num2)
+print("Multiplication of two numbers: ", result)
